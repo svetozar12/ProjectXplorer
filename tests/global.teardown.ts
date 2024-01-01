0dc95a9491de3be1066bfc171428ce5203a889de
svetozar12 <s.gospodinov02@gmail.com>
2024-01-01 08:05:42 +0200

    stuff

--- tests/global.teardown.ts
+++ tests/global.teardown.ts
@@ -1,13 +1,17 @@
-import { dbDisconnect } from '../src/lib/server/mongo';
-
-if (process.env.NODE_ENV !== 'test') {
-	// Only import the file if the environment is not 'test'
-	require('../scripts/unseed');
-}
+import { getDB, dbConnect } from '../src/lib/server/mongo';
 
 async function globalTeardown() {
-	console.log('db cleanup');
-	await dbDisconnect();
+	try {
+		if (process.env.NODE_ENV !== 'test') {
+			await dbConnect();
+			const db = await getDB();
+			console.log(db);
+			await db.dropDatabase();
+		}
+		console.log('db cleanup');
+	} catch (error) {
+		console.log(`Error in globalTeardown: ${error}`);
+	}
 }
 
 export default globalTeardown;
